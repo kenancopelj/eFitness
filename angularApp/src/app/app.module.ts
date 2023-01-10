1a41f4b9aa8c0f3b8d9d90d56abd0c94c6d5ba64
AdinMaksumic <adin.maksumic@edu.fit.ba>
2023-01-10 23:36:36 +0100

    pocetak angulara

--- angularApp/src/app/app.module.ts
+++ angularApp/src/app/app.module.ts
@@ -2,15 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import {HttpClientModule} from "@angular/common/http";
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import {FormsModule} from "@angular/forms";
+import { ShopComponent } from './shop/shop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    ShopComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+
 
   ],
   providers: [],
