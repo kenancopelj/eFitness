1f86e67541cee9ef0b218b8c0a745354e9366b15
Kenan Čopelj <kenan.copelj@edu.fit.ba>
2023-01-12 23:20:10 +0100

    dizajn aplikacije, add shop i clanarine

---
+++ angularApp/src/app/clanarine/clanarine.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clanarine',
+  templateUrl: './clanarine.component.html',
+  styleUrls: ['./clanarine.component.css']
+})
+export class ClanarineComponent {
+
+}
