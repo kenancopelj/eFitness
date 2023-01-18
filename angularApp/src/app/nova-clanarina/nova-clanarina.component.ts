5194077c4613c279ac05e6af9640f4725a65c78d
Kenan Čopelj <kenan.copelj@edu.fit.ba>
2023-01-18 17:10:32 +0100

    novi commit

---
+++ angularApp/src/app/nova-clanarina/nova-clanarina.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nova-clanarina',
+  templateUrl: './nova-clanarina.component.html',
+  styleUrls: ['./nova-clanarina.component.css']
+})
+export class NovaClanarinaComponent {
+
+}
