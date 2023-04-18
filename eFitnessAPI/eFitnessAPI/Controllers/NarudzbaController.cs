dd95c9f9618e310031c56842ec9cd922dac11cde
AdinMaksumic <adin.maksumic@edu.fit.ba>
2023-04-18 19:40:54 +0200

    df
    d

--- eFitnessAPI/eFitnessAPI/Controllers/NarudzbaController.cs
+++ eFitnessAPI/eFitnessAPI/Controllers/NarudzbaController.cs
@@ -35,6 +35,27 @@ namespace eFitnessAPI.Controllers
 
             return Ok();
         }
-        
+
+        //[HttpGet("{id}")]
+        //public IActionResult GetSuplementiByCategory(int id)
+        //{
+        //    var podaci = new List<Suplement>();
+        //    var Narudzbe = dbContext.Narudzba.ToList();
+        //    for (int i = 0; i < Narudzbe.Count() ; i++)
+        //    {
+        //        for (int j = 0; j < Narudzbe[i].Suplementi.Count(); j++)
+        //        {
+        //            podaci.Add(Narudzbe[i].Suplementi.ToList()[j]. as Suplement)
+
+        //        }
+        //    }
+            
+            
+            
+
+        //    return Ok();
+        //}
+
+
     }
 }
