ff3d0fbe735164eb379c21e91715f8e6b2f5bd79
AdinMaksumic <adin.maksumic@edu.fit.ba>
2023-04-21 00:11:04 +0200

    Update ReportService.cs

--- eFitnessAPI/eFitnessReport/Services/ReportService.cs
+++ eFitnessAPI/eFitnessReport/Services/ReportService.cs
@@ -25,35 +25,42 @@ namespace eFitnessReport.Services
             AspNetCore.Reporting.LocalReport report = new AspNetCore.Reporting.LocalReport(pathRdlc);
 
            
-                        var dateTimeFromInString = queryString.TryGetValue("df", out string df) ? df : DateTime.Now.ToString("dd.MM.yyyy");
-                        DateTime dateTimeFrom = DateTime.TryParse(dateTimeFromInString, out DateTime dFrom) ? dFrom : DateTime.Now;
+                        //var dateTimeFromInString = queryString.TryGetValue("df", out string df) ? df : DateTime.Now.ToString("dd.MM.yyyy");
+                        //DateTime dateTimeFrom = DateTime.TryParse(dateTimeFromInString, out DateTime dFrom) ? dFrom : DateTime.Now;
 
-                        var dateTimeToInString = queryString.TryGetValue("dt", out string dt) ? dt : DateTime.Now.ToString("dd.MM.yyyy");
-                        DateTime dateTimeTo = DateTime.TryParse(dateTimeToInString, out DateTime dTo) ? dTo : DateTime.Now;
-
+                        //var dateTimeToInString = queryString.TryGetValue("dt", out string dt) ? dt : DateTime.Now.ToString("dd.MM.yyyy");
+                        //DateTime dateTimeTo = DateTime.TryParse(dateTimeToInString, out DateTime dTo) ? dTo : DateTime.Now;
+                        
+                        long userId = queryString.TryGetValue("u", out var cuId) ? Convert.ToInt64(cuId) : 0;
+                        long narID = queryString.TryGetValue("k", out var kId) ? Convert.ToInt64(kId) : 0;
 
                         List<object> l = new List<object>();
 
-                        var podaci = await dBContext.Narudzba
-                            .Where(x => x.VrijemePravljenja >= dateTimeFrom && x.VrijemePravljenja <= dateTimeTo)
+                        var podaci = await dBContext.StavkeNarudzbe.Include(x => x.narudzba).Include(x => x.suplement)
+                            .Where(x => x.narudzba_id == narID && x.narudzba.korisnik_id == userId)
                             .ToListAsync();
 
-                        var ukupno = podaci.Sum(x => x.Total);
+                        var podaciNar = await dBContext.Narudzba.FindAsync(narID);
+
+                        double ukupno = 0;
 
                         for (int i = 0; i < podaci.Count; i++)
                         {
                             l.Add(new
                             {
                                 Rb = i + 1,
-                                VrijemePravljenja = podaci[i].VrijemePravljenja.ToString("dd.MM.yyyy"),
-                                Total = podaci[i].Total.ToString()
+                                Naziv = podaci[i].suplement.naziv,
+                                Kolicina = podaci[i].kolicina,
+                                Cijena = podaci[i].kolicina * podaci[i].suplement.cijena
                             });
+                             ukupno =+ podaci[i].kolicina * podaci[i].suplement.cijena;
                         }
                         var listOfParameters = new List<object>();
                         var _parameters = new{
                             Ukupno = ukupno.ToString(),
-                            DatumOd = dateTimeFrom.ToString("dd.MM.yyyy"),
-                            DatumDo = dateTimeTo.ToString("dd.MM.yyyy")
+                            KorisnikIme = dBContext.Korisnik.Find(userId).Ime,
+                            KorisnikPrezime = dBContext.Korisnik.Find(userId).Prezime,
+                            VrijemePravljenja = podaciNar.VrijemePravljenja.ToString("dd.MM.yyyy")
                         };
                         
 
